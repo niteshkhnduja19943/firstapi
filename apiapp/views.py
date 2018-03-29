f00dc32b924060887b11941f74c06dd11fb8101e
iamnitesh <niteshkhanduja19943@gmail.com>
2018-03-29 11:25:17 +0530

    initial commit method:- GET, POST,DELETE, SEND EMAIL via postman

--- apiapp/views.py
+++ apiapp/views.py
@@ -5,41 +5,31 @@ from rest_framework.permissions import AllowAny
 from rest_framework.mixins import status
 
 
-def send_email(email):
+def send_mail(stre, email):
     from django.core.mail import send_mail
+
     send_mail(
         'user login details',
-        'my msg',
+        stre,
         'kartik.bhatnagar@civilmachines.com',
         [email],
-        fail_silently=False,
-    )
-    return Response("email has been sent",status.HTTP_201_CREATED)
+
+        fail_silently=False, )
 
 
 class TestTableApi(APIView):
-    permission_classes =(AllowAny,)
+    permission_classes = (AllowAny,)
     renderer_classes = (JSONRenderer,)
 
+    def get(self):
 
+        import random
 
-
-    def get(self, request):
-
-
-        send_email("niteshkhanduja19943@gmail.com")
-
-        return Response("success",status.HTTP_202_ACCEPTED)
-
-
-
-
-
-
-
+        stre = str(random.randrange(1, 101))
+        send_mail(stre, 'niteshkhanduja19943@gmail.com')
+        return Response("success", status.HTTP_201_CREATED)
 
     def post(self,request):
-
         from .serializers import TestTableSerializers
         sdata = TestTableSerializers(data=request.data)
 
@@ -47,30 +37,20 @@ class TestTableApi(APIView):
             sdata.save()
             return Response(sdata.data, status=status.HTTP_201_CREATED)
         else:
+          return Response(sdata.errors, status=status.HTTP_400_BAD_REQUEST)
 
-
-            return Response(sdata.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-    def  delete(self,request):
+    def delete(self, request):
         from .serializers import Delete
-        sdata =Delete(data=request.data)
+        sdata = Delete(data=request.data)
         if sdata.is_valid():
             from .models import TestTable
             obj = TestTable.objects.get(pk=sdata.validated_data.get('id', 0))
             obj.delete()
-            return Response("Deleted Succesfully", status=status.HTTP_202_ACCEPTED)
+            return Response("Deleted Successfully", status=status.HTTP_202_ACCEPTED)
         else:
             return Response(sdata.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    def forgot_id(self, request):
-        from .serializers import forgot_id
-        sdata = forgot_id(data=request.data)
-        if sdata.is_valid():
-            from .serializers import TestTableSerializers
-            return Response(sdata.data, status=status.HTTP_202_ACCEPTED)
-        else:
-            send_email("niteshkhanduja19943@gmail.com")
+
 
 
 
